1b2ef2556ee6f4768f42787b883d871a0f0ff17b
minsulee2 <minsu@comcom.ai>
2021-03-22 19:14:14 +0900

    Add Styled components

--- src/components/HistoryTable.js
+++ src/components/HistoryTable.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const HistoryTableWrapper = styled.div`
+  width: 512px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 6rem;
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const Title = styled.div`
+  background: #8b3eeb;
+  color: white;
+  height: 4rem;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  background: white;
+`;
 
 const HistoryTable = () => {
   return (
-    <div className="HistoryTable">
-      <div className="app-title">Comcom History</div>
-      <div className="content"></div>
-    </div>
+    <HistoryTableWrapper>
+      <Title>Comcom History</Title>
+      <Content />
+    </HistoryTableWrapper>
   )
 }
 
